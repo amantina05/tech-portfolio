783363861df7e4c3996f9135e65485f0e0ef8f8d
Amantina Sosa <44170761+amantina05@users.noreply.github.com>
2020-10-02 15:23:24 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Amantina Sosa', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://amam.github.io', // Your website URL
+    url: 'https://amamntina05.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
